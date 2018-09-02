1c882d6af795c900272fc77924c2338be178b2db
gf <feng.gao.xatu@foxmail.com>
2018-09-03 00:52:04 +0800

    数字-字母编码方式

--- NumDecoding.cpp
+++ NumDecoding.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <vector>
 #include<string>
 #include<iostream>
 
@@ -7,6 +8,9 @@ using namespace std;
 //现在给一串数字，给出有多少种可能的译码结果。
 //例如：12;输出：2( ab 和 l );
 //--思路--：分为两种情况：1:单个数字字符对应一种；2：连续两个数字字符在1到26之间，
+//还是动态规划：
+//dp[i] = dp[i-1]+dp[i-2] 条件：i>2&&(str[i-2]=='1'||str[i-2]=='2'&&str[i-1]<='6')
+//dp[i] = dp[i-1]+1 条件：i==2
 int numdecodings(const string& str)
 {
   if(str.size() < 1 || str[0]=='0')
@@ -28,6 +32,29 @@ int numdecodings(const string& str)
   return first;
 }
 
+int numdecoding_2(const string& str)
+{
+  int len = str.size();
+  vector<int> dp;
+  dp.resize(len+1, 0);
+  dp[0] = 0;
+  dp[1] = 1;
+  if(str.size() < 1||str[0] == '0')
+    return 0;
+  for(int i = 2; i <= len; ++i)
+  {
+    if(str[i-1 != '0'])
+      dp[i] = dp[i-1];
+    if(str[i-2]=='1'||(str[i-2]=='2'&&str[i-1]<='6'))
+    {
+      if(i==2)
+        dp[i] += 1;
+      else 
+        dp[i] += dp[i-2];
+    }
+  }
+  return dp[len];
+}
 int main()
 {
   string str;
